f6484124df03d41e5ba06181b8e180592c88c559
Magnetika <agneslilla.szabo@gmail.com>
2025-09-24 19:20:45 +0200

    add adopt.html and adopters.html

--- src/controller/events.js
+++ src/controller/events.js
@@ -1,12 +1,21 @@
+// events.js
+
 import { loadLanguage } from "../i18n/i18n.js";
 
 export function initEvents() {
-  document.addEventListener("click", (e) => {
-    if (e.target.matches("[data-setlang]")) {
-      const currentButtonLang = e.target.getAttribute("data-setlang");
-      const newLang = currentButtonLang === "hu" ? "en" : "hu";
-      loadLanguage(newLang);
-      e.target.setAttribute("data-setlang", newLang);
-    }
-  });
+    document.addEventListener("click", (e) => {
+        if (e.target.matches("[data-setlang]")) {
+            const currentButtonLang = e.target.getAttribute("data-setlang");
+            const newLang = currentButtonLang === "hu" ? "en" : "hu";
+            loadLanguage(newLang);
+            e.target.setAttribute("data-setlang", newLang);
+        }
+    });
+
+    const flipCards = document.querySelectorAll('.flip-card');
+    flipCards.forEach(card => {
+        card.addEventListener('click', () => {
+            card.classList.toggle('is-flipped');
+        });
+    });
 }
