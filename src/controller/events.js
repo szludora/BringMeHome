4d03a01577299fc9170921ee84980ca2927bff3a
Magnetika <agneslilla.szabo@gmail.com>
2025-09-25 16:39:35 +0200

    resolved prev comments

--- src/controller/events.js
+++ src/controller/events.js
@@ -11,11 +11,4 @@ export function initEvents() {
             e.target.setAttribute("data-setlang", newLang);
         }
     });
-
-    const flipCards = document.querySelectorAll('.flip-card');
-    flipCards.forEach(card => {
-        card.addEventListener('click', () => {
-            card.classList.toggle('is-flipped');
-        });
-    });
 }
