6d4bc6d412ce0ee39be6c24226f079cfa2ec7b2e
Magnetika <36668713+Magnetika@users.noreply.github.com>
2025-09-25 20:47:35 +0200

    feat: BMH-13 BMH-14 Adopt me / Adopters section (#21)
    * i added the a basic adopt me section and a basic css
    
    * added more cat pictures
    
    * html files
    
    * remove empty modification
    
    * add adopt.html and adopters.html
    
    * resolved prev comments
    
    * changes

--- src/controller/events.js
+++ src/controller/events.js
@@ -1,12 +1,14 @@
+// events.js
+
 import { loadLanguage } from "../i18n/i18n.js";
 
 export function initEvents() {
-  document.addEventListener("click", (e) => {
-    if (e.target.matches("[data-setlang]")) {
-      const currentButtonLang = e.target.getAttribute("data-setlang");
-      const newLang = currentButtonLang === "hu" ? "en" : "hu";
-      loadLanguage(newLang);
-      e.target.setAttribute("data-setlang", newLang);
-    }
-  });
+    document.addEventListener("click", (e) => {
+        if (e.target.matches("[data-setlang]")) {
+            const currentButtonLang = e.target.getAttribute("data-setlang");
+            const newLang = currentButtonLang === "hu" ? "en" : "hu";
+            loadLanguage(newLang);
+            e.target.setAttribute("data-setlang", newLang);
+        }
+    });
 }
